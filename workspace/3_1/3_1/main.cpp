4de7dceff6a44865a07f8d69b43fa7ab9c7272da
DestinHan <seung4530@naver.com>
2025-01-20 14:59:42 -0500

    UPDATED

---
+++ workspace/3_1/3_1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+    
+    int x = (4 + 2) * 3;    // Associativity, x is memory, after calculate all in right, then it assigned to left x
+    
+    int y = -3; // Unary minus
+    
+    cout << x << endl;
+    
+    int z = pow(2, 3); // 2 to the power(square) of 3
+    
+    cout << z << endl;
+    
+    return 0;
+}

de20e676bff2e56fcadac7f71312204f85ddb2d3
DestinHan <seung4530@naver.com>
2025-01-22 15:50:55 -0500

    UPDATED

---
+++ workspace/Logical_operator/Logical_operator/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int main(void) {
+    
+    using namespace std;
+    
+    // logical AND
+//    bool x = true;
+//    bool y = false;
+//    
+//    cout << (x && y) << endl;
+    
+//    bool hit = true;
+//    int health = 10;
+//    
+//    if (hit == true && health < 20) {
+//        cout << "die" << endl;
+//    }
+    
+    // logical OR
+//    bool x = true;
+//    bool y = false;
+//    
+//    cout << (x || y) << endl;
+    
+//    int v = 1;
+//    
+//    if (v == 0 || v == 1)
+//        cout << "v is 0 or 1" << endl;
+    
+    // short circuit evaluation
+//    int x = 1;
+//    int y = 2;
+//    
+//    if (x == 1 && y++ == 2) {
+//        // do something
+//    }
+//    
+//    cout << y << endl;  // y = 3... if x = 2 then y = 2
+    
+//    // De Morgan's Law
+//    bool x = true;
+//    bool y = false;
+//    
+//    !(x && y);
+//    !x || !y;
+    
+    // XOR
+    // false false false
+    // false true true
+    // true false true
+    // true true false
+    
+    return 0;
+}

0a3ba48d3738d81a5c03d3d269083c0eefd056ea
DestinHan <seung4530@naver.com>
2025-01-31 11:10:43 -0500

    UPDATED

---
+++ workspace/controlFlow/controlFlow/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(void) {
+    
+    using namespace std;
+    
+    cout << "I love you " << endl;
+    
+    exit(0);    // it can be 0 or 1 or 2 or...
+//    return 0;
+    
+    cout << "I love you, too" << endl;  // code will never be executed
+    
+    return 0;
+}

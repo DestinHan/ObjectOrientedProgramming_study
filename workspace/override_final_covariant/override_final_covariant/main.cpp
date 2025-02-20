741485e7abcc9bb03966cab203b291c6fdea31c2
DestinHan <seung4530@naver.com>
2025-02-20 09:27:45 -0500

    UPDATED

---
+++ workspace/override_final_covariant/override_final_covariant/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class A {
+    
+public:
+    virtual void print(int x) const { std::cout << "A" << std::endl; }
+    
+};
+
+class B : public A {
+    
+public:
+    virtual void print(int x) const override { std::cout << "B" << std::endl; }
+    
+};
+
+class C : public B {
+    
+public:
+    virtual void print() final { std::cout << "C" << std::endl; }   // can't overrid in D
+    
+};
+
+class D : public C {
+    
+public:
+//    virtual void print() { std::cout << "D" << std::endl; }
+    
+};
+
+int main() {
+    
+    A a;
+    B b;
+    
+    A & ref = b;
+    ref.print(1);
+    
+}

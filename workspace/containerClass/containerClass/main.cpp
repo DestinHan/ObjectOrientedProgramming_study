7137a995cd01d7f42ea2b8e46a862ca70a876400
DestinHan <seung4530@naver.com>
2025-03-17 09:45:57 -0400

    UPDATED

---
+++ workspace/containerClass/containerClass/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+class IntArray {
+private:
+    int m_length = 0;
+    int *m_data = nullptr;
+    
+public:
+    // Constructors
+    // Destructors
+    
+};
+
+int main() {
+    
+    using namespace std;
+    // IntArray my_arr{ 1, 3, 5, 7, 9 }
+    // my_arr.insertBefore(10, 1);
+    // my_arr.remove(3);
+    // my_arr.push_back(13);
+    
+    std::vector<int> int_vec;
+    std::array<int, 10> int_arr;
+    
+    return 0;
+}

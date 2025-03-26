fa555965a4b7fc4598fb1666a57d9f4d1bd730dc
DestinHan <seung4530@naver.com>
2025-03-26 09:19:17 -0400

    UPDATED

--- workspace/polymorphism/polymorphism/main.cpp
+++ workspace/polymorphism/polymorphism/main.cpp
@@ -4,7 +4,6 @@
 #include <string>
 
 class Animal {
-    
 protected:
     std::string m_name;
     
@@ -13,14 +12,13 @@ public:
     
     std::string getName() { return m_name; }
     
-    virtual void speak() const {
+    virtual void speak() const {    // virtual made speak() goes down to child class(polymorphism_
         std::cout << m_name << " ? ? ? " << std::endl;
     }
     
 };
 
 class Cat : public Animal {
-    
 public:
     Cat(std::string name) : Animal(name) {}
     
@@ -31,7 +29,6 @@ public:
 };
 
 class Dog : public Animal {
-    
 public:
     Dog(std::string name) : Animal(name) {}
     
@@ -60,7 +57,7 @@ int main() {
     ptr_animal2->speak();
     
     Cat cats[] = {Cat("cat1"), Cat("cat2"), Cat("cat3"), Cat("cat4"), Cat("cat5")};
-    Dog dogs[] = {Dog("cat1"), Dog("cat2")};
+    Dog dogs[] = {Dog("dog1"), Dog("dog2")};
     
     for (int i=0; i<5; ++i) {
         cats[i].speak();
@@ -71,7 +68,7 @@ int main() {
     }
     
     Animal *my_animals[] = {&cats[0], &cats[1], &cats[2], &cats[3], &cats[4], &dogs[0], &dogs[1]};
-    for (int i=0; i<7; i++) {
+    for (int i=0; i<7; ++i) {
         my_animals[i] -> speak();
     }
     

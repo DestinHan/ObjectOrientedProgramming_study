d6d5e052c942c38103779e3c3c0c8db7d7a34027
DestinHan <seung4530@naver.com>
2025-04-05 14:30:29 -0400

    UPDATED

--- workspace/stdMove/stdMove/main.cpp
+++ workspace/stdMove/stdMove/main.cpp
@@ -1,5 +1,76 @@
-#include <iostream>
+#include "AutoPtr.h"
+#include "Resource.h"
+
+//AutoPtr<Resource> generateResource() {
+//    AutoPtr<Resource> res(new Resource(10000000));
+//    
+//    return res;
+//}
+
+template<typename T>
+void MySwap(T & a, T & b) {
+//    T tmp = a;
+//    a = b;
+//    b = tmp;
+    
+    T tmp = std::move(a);
+    a = b;
+    b = tmp;
+}
+
 int main() {
     
+    using namespace std;
     
+    streambuf* orig_buf  = cout.rdbuf();
+//    cout.rdbut(NULL);
+    
+//    Timer timer;
+    
+    {
+        AutoPtr<Resource> res1(new Resource(100000000));
+        
+        cout << res1.m_ptr << endl;
+        
+        AutoPtr<Resource> res2 = std::move(res1);       // using move semantics
+        cout << res1.m_ptr << endl;     // nullptr
+        cout << res2.m_ptr << endl;
+       
+    }
+    
+    {
+        AutoPtr<Resource> res1(new Resource(3));
+        res1->setAll(3);
+        
+        AutoPtr<Resource> res2(new Resource(5));
+        res2->setAll(5);
+        
+        res1->print();
+        res2->print();
+        
+        MySwap(res1, res2);
+        
+        res1->print();
+        res2->print();
+    }
+    
+    {
+        std::vector<string> v;
+        std::string str = "Hello";
+        
+        v.push_back(str);
+        
+        cout << str << endl;
+        cout << v[0] << endl;
+        
+        v.push_back(std::move(str));
+        
+        cout << str << endl;                // empty...
+        cout << v[0] << " " << v[1] << endl;
+    }
+    
+    cout.rdbuf(orig_buf);
+//    cout << timer.elapsed << endl;
+    
+    return 0;
 }
--- workspace/uniquePtr/uniquePtr/main.cpp
+++ workspace/uniquePtr/uniquePtr/main.cpp
@@ -2,7 +2,18 @@
 #include <memory>
 #include "Resource.h"
 
+auto doSomething() {
+//    auto res1 = std::make_unique<Resource>(5);
+//    return res1;
+    
+    return std::unique_ptr<Resource>(new Resource(5)); // move
+    
+//    return std::make_unique<Resource>(5);
+}
 
+void doSomething2(std::unique_ptr<Resource> & res) {
+    res->setAll(10);
+}
 
 int main() {
     
@@ -10,13 +21,48 @@ int main() {
     
     {
 //        Resource *res = new Resource(10000000);
-//        std::unique_ptr<Resource> res(new Resource(10000000));
+//        std::unique_ptr<Resource> res(new Resource(10000000));      // destory automatically
         
     }
     
     {
         std::unique_ptr<int> upi(new int);
+        
 //        auto *ptr = new Resource(5);
+//        std::unique_ptr<Resource> res1(new Resource(5));
+        auto res1 = std::make_unique<Resource>(5);
+        //auto res1 = doSomething();
+        
+        res1->setAll(5);
+        res1->print();
+        
+        std::unique_ptr<Resource> res2;
+        
+        std::cout << std::boolalpha;
+        std::cout << static_cast<bool>(res1) << std::endl;
+        std::cout << static_cast<bool>(res2) << std::endl;
+        
+//        res2 = res1;          // unique pointer can't copy, ONLY move semantics
+        res2 = std::move(res1);
+        
+        std::cout << std::boolalpha;
+        std::cout << static_cast<bool>(res1) << std::endl;
+        std::cout << static_cast<bool>(res2) << std::endl;
+        
+        if (res1 != nullptr) res1->print();
+        if (res2 != nullptr) res2->print(); // (*res2).print();
+    }
+    
+    {
+        auto res1 = std::make_unique<Resource>(5);
+        res1->setAll(1);
+        res1->print();
+        
+        doSomething2(res1);
+        //doSomething2(res1.get());
+        
+        std::cout << std::boolalpha;
+        res1->print();
     }
     
     return 0;

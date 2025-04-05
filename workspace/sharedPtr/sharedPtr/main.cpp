a041d6a4defc8cd7992b0d1a8aa078e3028bdb98
DestinHan <seung4530@naver.com>
2025-04-05 14:54:14 -0400

    UPDATED

--- workspace/sharedPtr/sharedPtr/main.cpp
+++ workspace/sharedPtr/sharedPtr/main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include "Resource.h"
+
+void doSomething(std::unique_ptr<Resource> res) {
+    
+}
 
 int main() {
     
     using namespace std;
     
+//    doSomething(std::make_unique<Resource>(1000000));
+//    Resource *res = new Resource(3);
+//    res->setAll(1);
     
+    {
+//        std::shared_ptr<Resource> ptr1(res);
+        
+        auto ptr1 = std::make_shared<Resource>(3);
+        ptr1->setAll(1);
+        
+        ptr1->print();      // 111
+        
+        {
+            std::shared_ptr<Resource> ptr2(ptr1);
+            //std::shared_ptr<Resource> ptr2(res);      // problem...
+            //auto ptr2 = ptr1;
+            
+            ptr2->setAll(3);
+            ptr2->print();  // 333
+            
+            std::cout << "Going out of the block" << std::endl;
+        }
+        ptr1->print();      // 333
+        
+        std::cout << "Going out of the outer block" << std::endl;
+    }
     
     return 0;
 }
